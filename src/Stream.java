728f2a01f6686b3d86ebbc984e7a6eb27bdbd350
gus <ggortiz09@gmail.com>
2021-06-13 23:15:32 -0300

    filter first name J

--- src/Stream.java
+++ src/Stream.java
@@ -18,13 +18,14 @@ public class Stream {
         Person p3 = new Person(3, "Juan Roman Riquelme", LocalDate.of(1980, 6, 23));
         Person p4 = new Person(4, "Emanuel ginobili", LocalDate.of(2019, 5, 15));
         Person p5 = new Person(5, "Martin Palermo", LocalDate.of(2010, 1, 4));
+        Person p6 = new Person(6, "Junior Roman Riquelme", LocalDate.of(1999, 6, 22));
 
         Product pr1 = new Product(1, "Quilmes", 150.0);
         Product pr2 = new Product(2, "Miller", 250.50);
         Product pr3 = new Product(3, "Heineken", 350.0);
         Product pr4 = new Product(4, "Budweiser", 250.0);
 
-        List<Person> persons = Arrays.asList(p1, p2, p3, p4, p5);
+        List<Person> persons = Arrays.asList(p1, p2, p3, p4, p5,p6);
         List<Product> products = Arrays.asList(pr1, pr2, pr3, pr4);
         
        /* for(int i = 0; i<persons.size(); i++){
@@ -63,7 +64,10 @@ public class Stream {
                    .ifPresent(System.out::println);
         
         System.out.println("*-*-*-*-*-*-*-*-");
-        
+        persons.stream()
+                  .filter(s -> s.getName().startsWith("J"))
+                  .forEach(System.out::println);
+                 
 	}
 
 	public static int getEdad(LocalDate birthDate) {
